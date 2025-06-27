cb2ff353f21409337c8c5da7c860b3ebd52c6466
Júlio César <juliocesaroalves679@gmail.com>
2025-06-27 11:33:23 -0300

    Melhoramento da função de exibição de informativos

--- static/js/exibicao_avisos.js
+++ static/js/exibicao_avisos.js
@@ -1,7 +1,19 @@
-/*
+
 const elementoExibicao = document.querySelector('.exibicaoAvisos');
 
 if (elementoExibicao) {
-    const texto = elementoExibicao.textContent;
-    elementoExibicao.innerHTML = texto;
-}*/
+    let texto = elementoExibicao.textContent;
+    texto = texto.replaceAll("''", '""');
+    texto = texto.replaceAll("'", '"');
+
+    const lista_avisos = JSON.parse(texto);
+    let avisos = ``;
+    for(const x of lista_avisos){
+        avisos = `<div class="estilo_aviso">
+                    <div class="estilo_data">${x['data']} ${x['hora']}</div>
+                    <div class="estilo_assunto">${String(x['assunto'])}</div>
+                    <div class="estilo_texto">${String(x['texto'])}</div>
+                 </div>`+avisos
+    }
+    elementoExibicao.innerHTML = avisos;
+}
