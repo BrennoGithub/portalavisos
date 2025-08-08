0f296f3167b8adccf763399f1d5f196b84109933
Júlio César <o.cesar@escolar.ifrn.edu.br>
2025-08-08 19:17:56 +0000

    Adição de comentarios

--- static/js/exibicaoInformativo.js
+++ static/js/exibicaoInformativo.js
@@ -1,12 +1,13 @@
 import { renderizaInformativos } from "./renderizacaoInformativos.js";
 
+//NÃO ESTAR CARREGANDO OS INFORMATIVOS NA PÁGINA COMUN. RESOLVER ESSE PROBLEMA.
+
 const exibicaoAvisos = document.querySelector('.exibicaoAvisos');
 const exibicaoAvalicoes = document.querySelector('.exibicaoAvaliacoes');
 const exibicaoMateriais = document.querySelector('.exibicaoMateriais');
 const exibicaoEventos = document.querySelector('.exibicaoEventos');
 
 document.addEventListener("DOMContentLoaded", async function(){
-    alert("ola mundo")
     await renderizaInformativos(exibicaoAvisos, "avisos" );
     await renderizaInformativos(exibicaoAvalicoes, "avaliacoes");
     await renderizaInformativos(exibicaoMateriais, "materiais");
