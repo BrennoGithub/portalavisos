0aaf6e1df6fca4de63977c958ea46bc33b0c05f9
Júlio César <juliocesaroalves679@gmail.com>
2025-08-28 17:40:33 -0300

    Atualização do layout dos informativos

--- static/js/renderizacaoInformativos.js
+++ static/js/renderizacaoInformativos.js
@@ -20,9 +20,8 @@ export async function renderizaInformativos(elemento, tipo) {
             let avisos = ``;
             for(const x of informativos){
                 avisos = `<div class="estilo_aviso">
-                    <div class="primeira_area  azul_1">${x['data']} ${x['hora']}</div>
                     <div class="segunda_area  azul_2">${String(x['assunto'])}</div>
-                    <div class="terceira_area  azul_3">${String(x['texto'])}
+                    <div class="terceira_area  azul_3">${String(x['texto'])} <em>${x['data']} - ${x['hora']}</em>
                         <div class="botoesEdit">
                         <img src="${STATIC_URL}icones/Delete.svg" alt="Icone Delete" class="icone_delete">
                         <img src="${STATIC_URL}icones/Edit.svg" alt="Icone Delete" class="icone_delete">
@@ -30,18 +29,23 @@ export async function renderizaInformativos(elemento, tipo) {
                     </div>
                 </div>`+avisos;
             };
-            avisos = `<div class="areaMaterial"><h2>Avisos</h2><hr> ${avisos} </div>`;
+            avisos = `
+            <div class="areaTitulo">
+                <h2>Avisos</h2><hr> 
+            </div>
+            <div class="areaMaterial">${avisos}</div>`;
             elemento.innerHTML = avisos;
             break;
 
         case "avaliacoes":
             let avaliacoes = ``;
             for(const x of informativos){
-                avaliacoes = `<div class="estilo_aviso">
-                        <div class="primeira_area verde_1">${x['data_avaliacao']} ${x['hora_avaliacao']}</div>
+                avaliacoes = `<div class="estilo_aviso">            
                         <div class="segunda_area  verde_2">${x['materia']}</div>
                         <div class="terceira_area  verde_3">
                             <strong>Assunto:</strong> ${String(x['assunto'])}
+                                <br>
+                            <strong>Horario:</strong> <em>${x['data_avaliacao']} - ${x['hora_avaliacao']}</em>
                                 <br>
                             ${String(x['descricao'])}
                             <div class="botoesEdit">
@@ -51,7 +55,11 @@ export async function renderizaInformativos(elemento, tipo) {
                         </div>
                     </div>`+avaliacoes;
             };
-            avaliacoes = `<div class="areaMaterial"><h2>Avaliações</h2><hr> ${avaliacoes} </div>`;
+            avaliacoes = `
+            <div class="areaTitulo">
+                <h2>Avaliações</h2><hr> 
+            </div>    
+            <div class="areaMaterial">${avaliacoes}</div>`;
             elemento.innerHTML = avaliacoes;
             break;
 
@@ -59,10 +67,11 @@ export async function renderizaInformativos(elemento, tipo) {
             let materiais = ``;
             for(const x of informativos){
                 materiais = `<div class="estilo_aviso">
-                        <div class="primeira_area laranja_1">${x['tipo_material']}</div>
                         <div class="segunda_area laranja_2">${x['materia']}</div>
                         <div class="terceira_area laranja_3">
                             <strong>Assunto:</strong> ${x['assunto']}
+                                <br>
+                            <strong>Tipo do Material:</strong> ${x['tipo_material']}
                                 <br>
                             ${String(x['material'])} ${String(x['descricao'])}
                             <div class="botoesEdit">
@@ -72,7 +81,11 @@ export async function renderizaInformativos(elemento, tipo) {
                         </div>
                     </div>`+materiais;
             };
-            materiais = `<div class="areaMaterial"><h2>Materiais</h2><hr> ${materiais} </div>`;
+            materiais = `
+            <div class="areaTitulo">
+                <h2>Materiais</h2><hr>
+            </div>
+            <div class="areaMaterial">${materiais}</div>`;
             elemento.innerHTML = materiais;
             break;
         
@@ -80,9 +93,11 @@ export async function renderizaInformativos(elemento, tipo) {
             let eventos = ``;
             for(const x of informativos){
                 eventos = `<div class="estilo_aviso">
-                         <div class="primeira_area roxo_1">${x['data_evento']} ${x['hora_evento']}</div>
                          <div class="segunda_area  roxo_2">${String(x['nome'])}</div>
-                         <div class="terceira_area  roxo_3">${String(x['descricao'])}
+                         <div class="terceira_area  roxo_3">
+                            <strong>Horário:</strong> <em>${x['data_evento']} ${x['hora_evento']}</em>
+                                <br>
+                            ${String(x['descricao'])}
                             <div class="botoesEdit">
                             <img src="${STATIC_URL}icones/Delete.svg" alt="Icone Delete" class="icone_delete">
                             <img src="${STATIC_URL}icones/Edit.svg" alt="Icone Delete" class="icone_delete">
@@ -90,7 +105,11 @@ export async function renderizaInformativos(elemento, tipo) {
                          </div>
                       </div>`+eventos;
             };
-            eventos = `<div class="areaMaterial"><h2>Eventos</h2><hr>${eventos}</div> `;
+            eventos = `
+            <div class="areaTitulo">
+                <h2>Eventos</h2><hr>
+            </div>
+            <div class="areaMaterial">${eventos}<div>`;
             elemento.innerHTML = eventos;
             break;
 
@@ -98,9 +117,8 @@ export async function renderizaInformativos(elemento, tipo) {
             let mural = ``;
             for(const x of informativos['avisos']){
                 mural = `<div class="estilo_aviso">
-                    <div class="primeira_area  azul_1">${x['data']} ${x['hora']}</div>
                     <div class="segunda_area  azul_2">${String(x['assunto'])}</div>
-                    <div class="terceira_area  azul_3">${String(x['texto'])}
+                    <div class="terceira_area  azul_3">${String(x['texto'])} <em>${x['data']} - ${x['hora']}</em>
                         <div class="botoesEdit">
                         <img src="${STATIC_URL}icones/Delete.svg" alt="Icone Delete" class="icone_delete">
                         <img src="${STATIC_URL}icones/Edit.svg" alt="Icone Delete" class="icone_delete">
@@ -111,10 +129,11 @@ export async function renderizaInformativos(elemento, tipo) {
 
             for(const x of informativos['avaliacoes']){
                 mural = `<div class="estilo_aviso">
-                        <div class="primeira_area verde_1">${x['data_avaliacao']} ${x['hora_avaliacao']}</div>
                         <div class="segunda_area  verde_2">${x['materia']}</div>
                         <div class="terceira_area  verde_3">
                             <strong>Assunto:</strong> ${String(x['assunto'])}
+                                <br>
+                            <strong>Horario:</strong> <em>${x['data_avaliacao']} - ${x['hora_avaliacao']}</em>
                                 <br>
                             ${String(x['descricao'])}
                             <div class="botoesEdit">
@@ -127,10 +146,11 @@ export async function renderizaInformativos(elemento, tipo) {
 
             for(const x of informativos['materiais']){
                 mural = `<div class="estilo_aviso">
-                        <div class="primeira_area laranja_1">${x['tipo_material']}</div>
                         <div class="segunda_area laranja_2">${x['materia']}</div>
                         <div class="terceira_area laranja_3">
                             <strong>Assunto:</strong> ${x['assunto']}
+                                <br>
+                            <strong>Tipo do Material:</strong> ${x['tipo_material']}
                                 <br>
                             ${String(x['material'])} ${String(x['descricao'])}
                             <div class="botoesEdit">
@@ -143,9 +163,11 @@ export async function renderizaInformativos(elemento, tipo) {
 
             for(const x of informativos['eventos']){
                 mural = `<div class="estilo_aviso">
-                         <div class="primeira_area roxo_1">${x['data_evento']} ${x['hora_evento']}</div>
                          <div class="segunda_area  roxo_2">${String(x['nome'])}</div>
-                         <div class="terceira_area  roxo_3">${String(x['descricao'])}
+                         <div class="terceira_area  roxo_3">
+                            <strong>Horário:</strong> <em>${x['data_evento']} ${x['hora_evento']}</em>
+                                <br>
+                            ${String(x['descricao'])}
                             <div class="botoesEdit">
                             <img src="${STATIC_URL}icones/Delete.svg" alt="Icone Delete" class="icone_delete">
                             <img src="${STATIC_URL}icones/Edit.svg" alt="Icone Delete" class="icone_delete">
@@ -153,7 +175,11 @@ export async function renderizaInformativos(elemento, tipo) {
                          </div>
                       </div>`+mural;
             };
-            mural = `<div class="areaMaterial"><h2>Mural</h2><hr>${mural}</div>`;
+            mural = `
+            <div class="areaTitulo">
+                <h2>Mural</h2><hr>
+            </div>
+            <div class="areaMaterial">${mural}</div>`;
             elemento.innerHTML = mural;
             break;
     };
