52f74aee9bbd92a349f368236b81d9d6b92445c8
BrennoGithub <breno.gusmao@escolar.ifrn.edu.br>
2025-09-26 18:40:08 +0000

    Lista de professores e materias completas

--- dados/lista_professores.py
+++ dados/lista_professores.py
@@ -1,11 +1,122 @@
 lista_professores = [ 
     {
-        "matricula": "",
-        "nome": "",
+        "matricula": "1210907",
+        "nome": "Janduir Egito da Silva",
         "nomeSocial": "",
         "aniversario": "",
-        "senhaSistema": "",
-        "materia":
+        "senhaSistema": "prof907",
+        "materia": "Química I"
     },
-
+    {
+        "matricula": "3441856",
+        "nome": "Antonio Douglas Sampaio Ramalho",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof856",
+        "materia": "Filosofia III"
+    },
+    {
+        "matricula": "1456703",
+        "nome": "Carlos Eduardo de Lima Duarte",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof703",
+        "materia": "Matemática III"
+    },
+    {
+        "matricula": "1814262",
+        "nome": "Jose Rauryson Alves Bezerra",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof262",
+        "materia": "Matemática III"
+    },
+    {
+        "matricula": "1934732",
+        "nome": "Ricardo Jose Vilar da Costa",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof732",
+        "materia": "História I"
+    },
+    {
+        "matricula": "1577142",
+        "nome": "Jurandy Martins Soares Junior",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof142",
+        "materia": "Arquitetura de Redes"
+    },
+    {
+        "matricula": "2664007",
+        "nome": "Alvaro Hermano da Silva",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof007",
+        "materia": "Orientação para a Prática Profissional II"
+    },
+    {
+        "matricula": "3330062",
+        "nome": "Andrea Pereira da Silva",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof062",
+        "materia": "Biologia I"
+    },
+    {
+        "matricula": "1814194",
+        "nome": "Alisson Diego Dias de Medeiros",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof194",
+        "materia": "Língua Portuguesa e Literatura III"
+    },
+    {
+        "matricula": "1071759",
+        "nome": "Daniel Aguiar da Silva Oliveira Carvalho",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof759",
+        "materia": "Autoria Web"
+    },
+    {
+        "matricula": "2664007",
+        "nome": "Alvaro Hermano da Silva",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof007",
+        "materia": "Desenvolvimento de Projeto Integrador"
+    },
+    {
+        "matricula": "1815949",
+        "nome": "Zulmar Jofli dos Santos Junior",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof949",
+        "materia": "Organização e Manutenção de Computadores"
+    },
+    {
+        "matricula": "3399615",
+        "nome": "Renan Alves de Morais Rocha",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof615",
+        "materia": "Programação com Acesso a Banco de Dados"
+    },
+    {
+        "matricula": "1071759",
+        "nome": "Daniel Aguiar da Silva Oliveira Carvalho",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof759",
+        "materia": "Orientação para a Prática Profissional I"
+    },
+    {
+        "matricula": "2476638",
+        "nome": "Lucia de Fatima Vieira da Costa",
+        "nomeSocial": "",
+        "aniversario": "",
+        "senhaSistema": "prof638",
+        "materia": "Sociologia III"
+    }
 ]
