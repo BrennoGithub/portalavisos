0c749011907f844fa1869f46dadf34abe1fd6642
Júlio César <juliocesaroalves679@gmail.com>
2025-09-28 13:44:07 -0300

    Adição de funcionalidade de login de alunos e professores em rota.py

--- rotas.py
+++ rotas.py
@@ -3,7 +3,6 @@ from flask import session, jsonify
 from CRUD.Create import criaInformativo
 from CRUD.Read import exibiInformativo
 from dados.lista_informativos import lista_id_informativos, lista_informativos
-from dados.validadeLogin import validadeLogin
 from dados.funcoesData import return_DataAtual
 from dados.lista_alunos import lista_alunos
 from modelos import *
@@ -27,7 +26,6 @@ def teste():
             "liderTurma": iten.liderTurma
         }
         lista_alunos.append(objetoAluno)
-    
     return jsonify(lista_alunos)
 
 
@@ -53,39 +51,86 @@ def returnTurma(ID_turma):
 
     return jsonify(turma)
 
-@app.route("/usuarios")
-def returnUSUARIOS():
-    return jsonify(lista_alunos)
+@app.route("/usuarios/<string:tipoUsuario>")
+def returnUSUARIOS(tipoUsuario):
+    match tipoUsuario:
+        case "aluno":
+            alunos = Alunos.query.all()
+            lista_alunos = []
+            for iten in alunos:
+                objetoAluno = {
+                    "matricula": iten.matricula,
+                    "nome": iten.nome,
+                    "nomeSocial": iten.nomeSocial,
+                    "aniversario": iten.aniversario,
+                    "senhaSistema": iten.senhaSistema,
+                    "liderTurma": iten.liderTurma,
+                    "turma": iten.turma
+                }
+                lista_alunos.append(objetoAluno)
+            return jsonify(lista_alunos)
+        
+        case "professor":
+            professores = Professores.query.all()
+            lista_professores = []
+            for iten in professores:
+                objetoProfessor = {
+                    "matricula": iten.matricula,
+                    "nome": iten.nome,
+                    "nomeSocial": iten.nomeSocial,
+                    "aniversario": iten.aniversario,
+                    "senhaSistema": iten.senhaSistema
+                }
+                lista_alunos.append(objetoProfessor)
+            return jsonify(lista_professores)
 
-@app.route("/usuarios/<string:matricula>")
-def returnUSUARIO(matricula):
+@app.route("/usuarios/<string:tipoUsuario>/<string:matricula>")
+def returnUSUARIO(tipoUsuario, matricula):
     if 'matricula' not in session or session['matricula'] != matricula:
         return redirect(url_for("login"))
     
-    if session["status"] == "False":
-        return render_template("tela_comun.html", nome = session["nomeUsuario"])
-    
-    elif session["status"] == "True":
-        return render_template("tela_lideres.html", nome = session["nomeUsuario"])
+    match tipoUsuario:
+        case "aluno":
+            if session["liderTurma"] == "False":
+                return render_template("tela_comun.html", nome = session["nomeUsuario"])
+            elif session["liderTurma"] == "True":
+                return render_template("tela_lideres.html", nome = session["nomeUsuario"])
+        case "professor":
+            return render_template("tela_lideres.html", nome = session["nomeUsuario"])
 
 @app.route("/submit_login", methods=["POST"])
 def valida_login():
     if request.method == "POST":
-        #ANALISAR PROBLEMA DE LOGIN DEPOIS DO LOGOUT
+        #ANALISAR COMO FAZER O LOGIN DE PROFESSORES
         matricula = request.form["matricula"]
         senha = request.form["senha"]
-
-        login = validadeLogin(lista_alunos, matricula, senha)
+        
+        usuario = ""
+        usuario = Alunos.query.get_or_404(matricula)
+        if usuario == None:
+            usuario = Professores.query.get_or_404(matricula)
+            if usuario == None:
+                usuario = "[ Matricula invalida ]"
+            else:
+                session["tipoUsuario"] = "professor"
+        else:
+            session["tipoUsuario"] = "aluno"
+        print(usuario)
+        
+        login = ""
+        if usuario.senhaSistema != senha:
+            login = "invalido"
+        else:
+            login = usuario.liderTurma
 
         if login == "invalido":
             return render_template("login.html")  
         else:
-            session["matricula"] = login["matricula"] # <-- Armazenados na sessão
-            session["nomeUsuario"] = login["nome"]
-            session["status"] = login["status"]
-            session["ID_turma"]  = login["ID_turma"]
-
-            return redirect(f'usuarios/{session["matricula"]}')
+            session["matricula"] = usuario.matricula # <-- Armazenados na sessão
+            session["nomeUsuario"] = usuario.nome
+            session["liderTurma"] = usuario.liderTurma
+            session["ID_turma"]  = usuario.turma
+            return redirect(f'usuarios/{session["tipoUsuario"]}/{session["matricula"]}')
             
     return redirect(url_for("/"))
 
