5833596e5239d8fbc00742e187bebe37b52ad739
Júlio César <o.cesar@escolar.ifrn.edu.br>
2025-12-09 17:46:59 +0000

    Adição de rota de erro e Começo da página Edit

--- FRONTEND/App.jsx
+++ FRONTEND/App.jsx
@@ -1,6 +1,6 @@
 import {GET} from "./static/js/requisicaoHTTP.js"
 import {useState, useEffect} from "react"
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Navigate, useNavigate} from "react-router-dom"
 import Login from "./Paginas/Login.jsx"
 import Inicial from "./Paginas/Inicial.jsx"
 import Form from "./Paginas/Form.jsx"
@@ -10,12 +10,14 @@ import "./static/css/estilo_Informativos.css"
 function App() {
   const [logado, setLogado] = useState(false)
   const [dadosUsuario, setDadosUsuario] = useState({})
+
   useEffect(() => {
     async function checkLogin(){
       const estaLogado = await GET("http://localhost:5000/");
       setDadosUsuario(estaLogado)
       setLogado(estaLogado["login"])
       "mensagemServidor" in estaLogado ? alert(estaLogado["mensagemServidor"]) : null
+      
     }
     checkLogin();
   }, []);
@@ -23,7 +25,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={logado ? <Inicial objetoUsuario={dadosUsuario}/> : <Navigate to={"http://localhost:5173/login"} replace/> }/>
+        <Route path="/" element={logado ? <Inicial objetoUsuario={dadosUsuario}/> : <Navigate to={"/login"} replace/> }/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/avisos" element={<Inicial info="avisos" tituloSessao="Avisos" objetoUsuario={dadosUsuario}/>}/>
         <Route path="/avaliacoes" element={<Inicial info="avaliacoes" tituloSessao="Avaliações" objetoUsuario={dadosUsuario}/>}/>
@@ -32,6 +34,7 @@ function App() {
         <Route path="/aniversariantes" element={<Inicial objetoUsuario={dadosUsuario}/>}/>
         <Route path="/form" element={<Form/>}/>
         <Route path="/informativos/:id" element={<h1>Turma</h1>}/>
+        <Route path="*" element={<h1>Página não encontrada</h1>}></Route>
       </Routes>
     </BrowserRouter>
   )
--- FRONTEND/Paginas/Edit.jsx
+++ FRONTEND/Paginas/Edit.jsx
@@ -1,12 +1,85 @@
-import React from "react";
 import BarraLateral from "../Compornentes/BarraLateral.jsx"
 import Corpo from "../Compornentes/Corpo.jsx"
+import {useState, useEffect} from "react"
+import {GET, PUT} from "../static/js/requisicaoHTTP.js"
+import "../static/css/estilo_login.css";
+import "../static/css/estilo_global.css";
+import "../static/css/estilo_formInformativos.css"
 
-function Edit(){
+function Edit({ID_informativo}){
+    const [form, setForm] = useState(null);
+    const [assuntoForm, setAssuntoForm] = useState("");
+
+    useEffect(() => {
+        async function BuscaInformativo(ID) {
+            const info = await GET(`${ID}`)
+            if ("mensagemServidor" in info){
+                alert(informativo["mensagemServidor"]);
+
+            } else{
+                return info;
+            }
+        };
+
+        const informativo = BuscaInformativo(ID_informativo);
+
+        switch (informativo["assunto"]){
+            case "Avaliação":
+                setForm(<>
+                Avaliação
+                </>)
+                setAssuntoForm(informativo["assunto"]);
+                break;
+            
+            case "Evento":
+                setForm(<>
+                Evento
+                </>)
+                setAssuntoForm(informativo["assunto"]);
+                break;
+
+            case "Material Didatico":
+                setForm(<>
+                Meterial
+                </>)
+                setAssuntoForm(informativo["assunto"]);
+                break;
+
+            default:
+                setForm(<>
+                Aviso
+                </>)
+                setAssuntoForm(informativo["assunto"]);
+                break;
+        }
+
+    }, []);
+
+    async function SubmitForm(event, tipoForm){
+        event.preventDefault();
+        let dadosForm = {}
+        switch (tipoForm){
+            case "": 
+                break;
+            case "": 
+                break;
+            case "": 
+                break;
+            default: 
+                break;
+        }
+        await PUT("", dadosForm);
+    };
+
     return (
         <>
             <BarraLateral liderTurma={true} nomeUsuario={"Júlio César"}/>
-            <Corpo titulo={tituloSessao}></Corpo>
+            <Cabecalho/>
+            <Corpo titulo={Edição}>
+                <form className="formAviso" onSubmit={async (event) => {SubmitForm(event, assuntoForm)}}>
+                    {form}
+                </form>
+            </Corpo>
         </>
     )
 }
